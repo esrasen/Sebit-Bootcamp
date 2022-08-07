fc40f0790d03398b0b7affb827589f2bc17b8697
Esra Sen <sen.esra@yahoo.com>
2022-08-07 19:24:45 +0300

    Add angular pipes and directives project

---
+++ Week-4-Angular-Directives/AngularPipe/AngularPipem/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  name:string = '';
+  date:string = '';
+  money:string = '';
+  height: number =0;
+
+
+  OnNameChange(value: string){
+this.name=value;
+  }
+
+  OnDateChange(value: string){
+    this.date=value;
+      }
+
+      OnMoneyChange(value: string){
+        this.money=value;
+      }
+
+
+      OnHeightChange(value: string){
+        this.height=parseFloat(value) ;
+      }
+
+}
